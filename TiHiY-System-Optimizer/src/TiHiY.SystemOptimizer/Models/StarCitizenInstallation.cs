7854921caa056db3938aa3f841f32e24827bd764
TiHiY-DED <234259815+sergsivak-lang@users.noreply.github.com>
2026-08-04 10:24:39 +0300

    Extend Star Citizen USER.cfg audit

--- TiHiY-System-Optimizer/src/TiHiY.SystemOptimizer/Models/StarCitizenInstallation.cs
+++ TiHiY-System-Optimizer/src/TiHiY.SystemOptimizer/Models/StarCitizenInstallation.cs
@@ -5,5 +5,8 @@ public sealed class StarCitizenInstallation
     public string Channel { get; init; } = "Невідомий канал";
     public string Path { get; init; } = string.Empty;
     public bool UserCfgExists { get; init; }
+    public int UserCfgActiveLines { get; init; }
+    public string? UserCfgLanguage { get; init; }
+    public DateTime? UserCfgLastWriteTime { get; init; }
     public string UserCfgPath => System.IO.Path.Combine(Path, "USER.cfg");
 }
